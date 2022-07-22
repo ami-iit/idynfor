7177bb9317859dc62163ee8ad88924f5fc133140
Silvio <silvio.traversaro@iit.it>
2022-07-22 16:46:45 +0200

    Add buildModelfromiDynTree function to load pinocchio model from iDynTree::Model
    As a first instance, we just support models composed by a single link.

--- test/iDynTreePinocchioConversionsTest.cpp
+++ test/iDynTreePinocchioConversionsTest.cpp
@@ -5,11 +5,12 @@
 
 // Catch2
 #include <catch2/catch_test_macros.hpp>
-
+#include <pinocchio/algorithm/model.hpp>
 #include <iDynFor/iDynTreePinocchioConversions.h>
 
 #include <iDynTree/Core/EigenHelpers.h>
 #include <iDynTree/Core/TestUtils.h>
+#include <iDynTree/Model/ModelTestUtils.h>
 
 // The serialization used by iDynTree and the one used by Pinocchio are different:
 // iDynTree: m mc_x mc_y mc_z I_xx I_xy I_xz I_yy I_yz I_zz
@@ -41,5 +42,34 @@ TEST_CASE("toPinocchio::iDynTree::SpatialInertia")
             linkDynamicParametersFromPinocchioToiDynTreeSerialization(pin_inertia_param);
         REQUIRE(idyn_inertia_param.isApprox(pin_inertia_param_idyn_serialization));
     }
+}
+
+
+TEST_CASE("buildModelfromiDynTree")
+{
+    // Seed the random generator used by iDynTree
+    srand(0);
+
+    for(size_t i=0; i < 10; i++) {
+        // For now just support 0-joint models
+        iDynTree::Model idynmodel = iDynTree::getRandomModel(0);
+        pinocchio::Model pinmodel;
+        bool verbose = true;
+        iDynFor::buildModelfromiDynTree(idynmodel, pinmodel, verbose);
+
+        REQUIRE(pinmodel.nbodies == idynmodel.getNrOfLinks());
+        REQUIRE(pinmodel.existBodyName(idynmodel.getLinkName(idynmodel.getDefaultBaseLink())));
+
+        // Verify that the inertia of the models match
+        iDynTree::SpatialInertia idyn_inertia = idynmodel.getLink(idynmodel.getDefaultBaseLink())->getInertia();
+        pinocchio::Inertia pin_inertia = pinmodel.inertias[0];
+
+        Eigen::Matrix<double,10,1> idyn_inertia_param = iDynTree::toEigen(idyn_inertia.asVector());
+        Eigen::Matrix<double,10,1> pin_inertia_param = pin_inertia.toDynamicParameters();
+        Eigen::Matrix<double,10,1> pin_inertia_param_idyn_serialization =
+            linkDynamicParametersFromPinocchioToiDynTreeSerialization(pin_inertia_param);
+        REQUIRE(idyn_inertia_param.isApprox(pin_inertia_param_idyn_serialization));
+    }
 
 }
+
